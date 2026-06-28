c28cdd6bfb47ea5dcc547888e1ef356cb13b0a1d
MinhHieuu <hieulm04z@gmail.com>
2026-06-28 10:04:50 +0700

    fix auth request handling

--- src/main/java/com/example/ats/application/dto/request/LoginRequest.java
+++ src/main/java/com/example/ats/application/dto/request/LoginRequest.java
@@ -1,10 +1,8 @@
 package com.example.ats.application.dto.request;
 
 import lombok.Getter;
-import org.springframework.stereotype.Service;
 
 @Getter
-@Service
 public class LoginRequest {
     private String email;
     private String password;
